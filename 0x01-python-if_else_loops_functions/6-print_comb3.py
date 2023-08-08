73d76dbed249a4a291f6f00b5f85c4d075b5f167
Joseph Abang <abangjosef@gmail.com>
2023-08-08 06:52:59 -0700

    Changed fstrings to str.format

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,7 +3,6 @@ for num in range(100):
     num1 = num // 10
     num2 = num % 10
     if num == 89:
-        print(f'{num}')
+        print('{}'.format(num))
     elif num1 < num2:
-        print(f'{num1}{num2}, ', end='')
-
+        print('{}{}, '.format(num1, num2), end='')
