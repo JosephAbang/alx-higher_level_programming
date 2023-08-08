221597daf19f66b708cfa50c45d5df4aaff3aa20
Joseph Abang <abangjosef@gmail.com>
2023-08-08 09:31:16 -0700

    Modified the uppercase function

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -2,9 +2,9 @@
 def uppercase(str):
     for char in str:
         ascii_code = ord(char)
-    if 97 <= ascii_code <=122:
-        uppercase_char = chr(ascii_code - 32)
-    else:
-        uppercase_char = char
-    print(uppercase_char, end='')
-print()
+        if 97 <= ascii_code <= 122:
+            upper_char = chr(ascii_code - 32)
+        else:
+            upper_char = char
+        print(upper_char, end='')
+    print()
