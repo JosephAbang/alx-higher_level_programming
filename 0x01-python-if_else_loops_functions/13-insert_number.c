1598650c0ce898d2c2d8468f570a28402c35cbb8
Joseph Abang <abangjosef@gmail.com>
2023-08-08 14:54:22 -0700

    Technical interview: insert a node

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * insert_node - Inserts a node
+ * @head: pointer to pointer of first node of listint_t list
+ * @number: integer to be included in new node
+ * Return: address of the new element or NULL if it fails
+ **/
+
+listint_t *insert_node(listint_t **head, int number)
+{
+	listint_t *new_node;
+	listint_t *current_node = *head;
+
+	while (current->next && (current_node->next->n < number))
+	{
+		current->node = current_node->next;
+	}
+	new_node = malloc(sizeof(listint_t));
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->n = number;
+	new_node->next = current_node->next;
+	current_node->next = new_node;
+
+	return (new_node);
+}
