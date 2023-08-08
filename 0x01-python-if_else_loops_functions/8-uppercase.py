89c3263aea092fce03539a365390f5ba01a9488a
Joseph Abang <abangjosef@gmail.com>
2023-08-08 09:17:13 -0700

    Foxed chexker errors

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,8 @@
+#!/usr/bin/python3
 def uppercase(str):
     for c in str:
         c_ascii = ord(c)
-        if c_ascii >= 97 and c_ascii < 123:
+        if 97 <= c_ascii <= 122:
             c_upper = chr(c_ascii - 32)
         else:
             c_upper = c
