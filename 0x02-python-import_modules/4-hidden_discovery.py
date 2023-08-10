1c6c93c20c7c9001a2b68407eb4acbf2a000f67a
Joseph Abang <abangjosef@gmail.com>
2023-08-10 11:24:18 -0700

    Removed whitespace errors

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4
-    
+    import hidden_4    
     _dir = dir(hidden_4)
-    
     for i in _dir:
         if i[:2] != "__":
             print(i)
