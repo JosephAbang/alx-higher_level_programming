b0e76e4e232bdea75eb801c19443a5c40c81e9f7
JosephAbang <abangjosef@gmail.com>
2023-08-12 10:00:53 +0100

    fixed bug: retrieve an element from a list like in C

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
+    if idx < 0 or idx >= len(my_list):
         return None
-    def find_item(b):
-        if my_list[idx] == b:
-            return b
-        else:
-            return 0
-    found = sum(list(map(find_item, my_list)))
-    return (found)
+    for item in my_list:
+        if my_list[idx] == item:
+            return item
