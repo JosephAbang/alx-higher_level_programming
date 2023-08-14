6716b6942f5912f7d35b849ffa6e68f54e9ef2c9
Joseph Abang <abangjosef@gmail.com>
2023-08-14 04:24:48 -0700

    print some basic info about Python lists

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <Python.h>
+
+/**
+ * print_python_list_info - Print info of python list
+ * @p: Struct pointer containing data
+ * Return: void
+ **/
+
+void print_python_list_info(PyObject *p)
+{
+	int size = Py_SIZE(p), i;
+	int alloc = ((PyListObject *)p)->allocated;
+	PyObject *item;
+
+	printf("[*] Size of the Python List = %d\n", size);
+	printf("[*] Allocated = %d\n", alloc);
+
+	for (i = 0; i < size; i++)
+	{
+		item = PyList_GET_ITEM(p, i);
+		printf("Element %d:  %s\n", i, Py_TYPE(item)->tp_name);
+	}
+}
