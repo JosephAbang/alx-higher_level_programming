8a93a2150c0f035b477ed0a7b58ecc0361ef8ef6
Joseph Abang <abangjosef@gmail.com>
2023-08-17 08:29:18 -0700

    Insert node at nth position

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,38 +2,37 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *temp = *head, *new_node;
+	dlistint_t *temp1 = *h, *temp2 = *h, *new_node;
 	unsigned int cnt;
 
+	while (temp1)
+	{
+		temp1 = temp1->next;
+		cnt++;	
+	}
+	if (idx > cnt)
+		return (NULL);
+	else if (idx == 0)
+		return (add_dnodeint(h, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return NULL;
-	new_node->n = n;
-	while (temp)
+	temp1 = *h;
+	while (idx != 1)
 	{
-		if (idx == cnt)
-		{
-			if (temp->next == NULL)
-			{
-				new_node->next = NULL;
-				new_node->prev = temp;
-				temp->next = new_node;
-			}
-			else if (temp->prev == NULL)
-			{
-				new_node->next = temp;
-				new_node->prev = NULL;
-				temp->next = NULL;
-				temp->prev = new_node;
-				*head = new_node
-			}
-			else
-			{
-				new_node->next = temp;
-				new_node->prev = temp->prev;
-				temp->prev = new_node;
-			}
-		}
-		temp = temp->next;
+		temp1 = temp1->next;
+		idx--;
 	}
+	if (temp1->next == NULL)
+		return (add_dnodeint_end(h, n));
+	else
+	{
+		new_node->n = n;
+		temp2 = temp1->next;
+		new_node->prev = temp1;
+		new_node->next = temp2;
+		temp2->prev = new_node;
+		temp1->next = new_node;
+	}
+	return (new_node);
 }
