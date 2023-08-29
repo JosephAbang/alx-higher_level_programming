9937ead5be2735a8bfbd91a54e7937745c315072
JosephAbang <abangjosef@gmail.com>
2023-08-29 09:43:51 +0100

    Debug safe-print function

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,12 +6,11 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
         return True
-    except (ValueError, TypeError):
-        try:
-            int(value)
-            print("Exception: Unknown format code 'd' for object of type '{}'\
-                    ".format(value.__class__.__name__), file=sys.stderr)
-            return False
-        except (ValueError, TypeError):
-            print("Exception: unsupported format string passed to str.__format__", file=sys.stderr)
-            return False
+    except TypeError:
+        print("Exception: Unknown format code 'd' for object of type '{}'\
+                ".format(value.__class__.__name__), file=sys.stderr)
+        return False
+    except ValueError:
+        print("Exception: unsupported format string passed to \
+                str.__format__", file=sys.stderr)
+        return False
