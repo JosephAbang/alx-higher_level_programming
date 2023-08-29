8b6b9ec65c5629826b45b3ad817400d6e23cce7e
JosephAbang <abangjosef@gmail.com>
2023-08-29 09:57:05 +0100

    Debug safe-print function: Switch exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,7 +7,7 @@ def safe_print_integer_err(value):
         int_val = int(value)
         print("{:d}".format(int_val))
         return True
-    except (ValueError, TypeError):
+    except ValueError:
         print("Exception: Unknown format code 'd' for object of type '{}'\
                 ".format(value.__class__.__name__), file=sys.stderr)
         return False
