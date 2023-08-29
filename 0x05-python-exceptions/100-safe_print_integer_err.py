4c5d2a63c5c9cbb1ac4d78c9e74ccc9ab8564324
JosephAbang <abangjosef@gmail.com>
2023-08-29 09:54:39 +0100

    Debug safe-print function: Swite exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,11 +7,11 @@ def safe_print_integer_err(value):
         int_val = int(value)
         print("{:d}".format(int_val))
         return True
-    except TypeError:
+    except (ValueError, TypeError):
         print("Exception: Unknown format code 'd' for object of type '{}'\
                 ".format(value.__class__.__name__), file=sys.stderr)
         return False
-    except ValueError:
+    except Exception:
         print("Exception: unsupported format string passed to \
                 {}.__format__".format(value.__class__.__name__), file=sys.stderr)
         return False
