3ea8a6848b8211f0c9b99141da728327fd6d5be9
JosephAbang <abangjosef@gmail.com>
2023-08-29 10:17:36 +0100

    Debug safe-print function: Switch exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -14,9 +14,3 @@ def safe_print_integer_err(value):
         print("Exception: unsupported format string passed to {}.__format__\
 ".format(value.__class__.__name__), file=sys.stderr)
         return False
-
-
-value = '89'
-has_been_print = safe_print_integer_err(value)
-if not has_been_print:
-    print("{} is not an integer".format(value))
