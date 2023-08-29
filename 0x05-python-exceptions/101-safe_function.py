cbc95e78b6f244ac3b6c381b06a59d3614ad66a9
JosephAbang <abangjosef@gmail.com>
2023-08-29 10:42:45 +0100

    Debug safe function execution: Use Exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -8,9 +8,10 @@ def safe_function(fct, *args):
     except IndexError:
         print("Exception: list index out of range", file=sys.stderr)
         result = None
-        pass
     except ZeroDivisionError:
         print("Exception: division by zero", file=sys.stderr)
         result = None
-        pass
+    except Exception as e:
+        print("Exception:", e, file=sys.stderr)
+        result = None
     return result
