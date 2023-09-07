6b9f849f19f327b6fbfa73f2d0b436b8da4a485d
JosephAbang <abangjosef@gmail.com>
2023-09-08 00:32:52 +0100

    Modified function

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -5,13 +5,36 @@
 def matrix_mul(m_a, m_b):
     """
     function that performs Matrix multiplication - only
-    Matrix product (two matrices)"""
-    if not m_a or not m_b:
-        return []
+    Matrix product (two matrices)
+    """
+    if not isinstance(m_a, list):
+        raise TypeError("m_a must be a list")
+
+    if not isinstance(m_b, list):
+        raise TypeError("m_a must be a list")
+
+    if not m_a:
+        raise ValueError("m_a can't be empty")
+    
+    if not m_b:
+        raise ValueError("m_b can't be empty")
+
+    if not all(isinstance(row, list) for row in m_a):
+        raise TypeError("m_a should contain only lists")
+
+    if not all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_b should contain only lists")
+
+    if len(set(len(row) for row in m_a)) != 1:
+        raise TypeError("each row of m_a must have the same size")
+
+    if len(set(len(row) for row in m_b)) != 1:
+        raise TypeError("each row of m_b must have the same size")
 
     if len(m_a[0]) != len(m_b):
         raise ValueError("Incompatible matrix dimensions for multiplication")
     new_matrix = []
+
     i = 0
     for lst in range(len(m_a)):
         new_list = []
