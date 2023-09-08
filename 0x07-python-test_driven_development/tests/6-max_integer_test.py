9fd40ec6694c0590fafc7b9338d5cead64b27939
JosephAbang <abangjosef@gmail.com>
2023-09-08 01:03:32 +0100

    Add shebang

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
+"""Unittest for max_integer([..])
+"""
 import unittest
-"""Unittest module for max integer"""
 max_integer = __import__('6-max_integer').max_integer
+
+
 
 class TestMaxInteger(unittest.TestCase):
     """Testing edge cases for max integer"""
