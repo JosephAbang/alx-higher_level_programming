ca629b096d8cc5606409fa7f976549b00311ab15
JosephAbang <abangjosef@gmail.com>
2023-09-13 03:30:58 +0100

    only attribute names contained in this list must be retrieved

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module contains a class"""
+
+
+class Student():
+    """creates instances of student class"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        if attrs is not None:
+            attr_dict = {}
+            for item in attrs:
+                if item in vars(self).keys():
+                    attr_dict[item] = vars(self)[item]
+            return attr_dict
+        else:
+            return vars(self)
