92313ab669b4f2d2594e97bca3f77d6bb7307277
Joseph Abang <abangjosef@gmail.com>
2023-10-19 16:56:38 -0700

    List all genres with ratings usine one SELECT

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -2,14 +2,12 @@
 
 
 SELECT name, SUM(rate) rating
-FROM tv_shows tvs
+FROM (tv_shows tvs
 INNER JOIN tv_show_ratings
-ON tvs.id = show_id
-INNER JOIN (SELECT name, tvsg.show_id
-	FROM tv_show_genres tvsg
-	JOIN tv_genres tvg
-	ON tvsg.genre_id = tvg.id
-) tvl
-ON tvs.id = tvl.show_id
+ON tvs.id = show_id)
+INNER JOIN (tv_show_genres tvsg
+JOIN tv_genres tvg
+ON tvsg.genre_id = tvg.id)
+ON tvs.id = tvsg.show_id
 GROUP BY name
 ORDER BY rating DESC;
