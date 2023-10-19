af72b9da9ea3b35e40a590976d7888aae27afdc2
Joseph Abang <abangjosef@gmail.com>
2023-10-19 10:45:45 -0700

    Query to list shows without genre

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+--  script that lists all shows contained in hbtn_0d_tvshow
+
+SELECT tvs.title, tvsg.genre_id 
+FROM tv_shows tvs
+LEFT JOIN tv_show_genres tvsg
+ON tvs.id = tvsg.show_id
+WHERE tvsg.genre_id IS NULL
+ORDER BY tvs.title, tvsg.genre_id;
