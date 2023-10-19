49c9180901a85d4de848972b218c16ed1fe12eaf
Joseph Abang <abangjosef@gmail.com>
2023-10-19 10:29:26 -0700

    Query to list show IDs by genre ID

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,7 @@
+--  script that lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
+
+SELECT tvs.title, tvsg.genre_id 
+FROM tv_shows tvs
+INNER JOIN tv_show_genres tvsg
+ON tvs.id = tvsg.genre_id
+ORDER BY tvs.title, tvsg.genre_id;
