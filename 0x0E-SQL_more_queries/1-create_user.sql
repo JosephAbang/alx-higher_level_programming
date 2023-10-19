fecfe90768eb34891982572a6490eb86c620c7ce
Joseph Abang <abangjosef@gmail.com>
2023-10-19 07:43:46 -0700

    Grant all privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Script createes a mysql user
 
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
