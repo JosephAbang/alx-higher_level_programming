65062d3fc86d510fd8d48c24d116acd320d38968
JosephAbang <abangjosef@gmail.com>
2023-11-13 22:10:09 +0100

    print  a message depending of the number of arguments passed

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const args = process.argv;
+
+if (args[3]) {
+  console.log('Arguments found');
+} else if (args[2]) {
+  console.log('Argument found');
+} else {
+  console.log('no argument');
+}
