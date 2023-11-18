b4f1cdeaf76db43296764f5b134e4335dcac5fbb
root <root@DESKTOP-KML44A3.localdomain>
2023-11-18 11:11:03 +0100

    Return filtered list

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""script returns filtered list"""
+
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    args = sys.argv
+
+    usr = args[1]
+    pwd = args[2]
+    db = args[3]
+    ht = "localhost"
+    pt = 3306
+
+    con = MySQLdb.connect(host=ht, user=usr, passwd=pwd, database=db, port=pt)
+    curs = con.cursor()
+    result = curs.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    rows = curs.fetchall()
+    for row in rows:
+        print(row)
