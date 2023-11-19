bac3ec8812d1ccc05f17e4ee14b872d25436bb2e
root <root@DESKTOP-KML44A3.localdomain>
2023-11-19 11:20:56 +0100

    Add new object

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Prints object matched with argument given
+"""
+
+from sqlalchemy.orm import Session
+from sys import argv
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+
+if __name__ == '__main__':
+    fmt_string = 'mysql+mysqldb://{}:{}@localhost/{}'
+    fmt = fmt_string.format(argv[1], argv[2], argv[3], pool_pre_ping=True)
+    engine = create_engine(fmt)
+    Base.metadata.create_all(engine)
+
+    session = Session(engine)
+    new_col = State(name='Louisiana')
+    session.add(new_col)
+    session.commit()
+    print(f"{new_col.id}")
+    session.close()
