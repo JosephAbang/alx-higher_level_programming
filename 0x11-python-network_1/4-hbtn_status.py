76fdd8a7f598f1642d7337481ff23294e0343125
root <root@DESKTOP-KML44A3.localdomain>
2023-12-02 01:50:30 +0100

    Fetch a ur;

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+"""
+
+import requests
+
+if __name__ == "__main__":
+    r = requests.get('https://alx-intranet.hbtn.io/status')
+    content = r.text
+    print('Body response:')
+    print(f'\t- type: {type(content)}')
+    print(f'\t- content: {content}')
