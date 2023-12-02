086425987305e50d7df60abfda53132257dcf1e3
root <root@DESKTOP-KML44A3.localdomain>
2023-12-02 02:44:35 +0100

    post responsand catch es in json

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+script posts responses
+"""
+
+import sys
+import requests
+
+if __name__ == "__main__":
+    url = 'http://0.0.0.0:5000/search_user'
+    if len(sys.argv) < 2:
+        char = ''
+    else:
+        char = sys.argv[2]
+    
+    val = {'q': char}
+    r = requests.post(url, data=val)
+    try:
+        new_json = r.json()
+        if new_json:
+            print(f"[{new_json.get('id')}] {new_json.get('name')}")
+        else:
+            print("No result")
+    except ValueError as e:
+        print("Not a valid JSON")
