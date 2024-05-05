90d50effa8d337216697f071c1a909b46b7875d0
JosephAbang <abangjosef@gmail.com>
2024-05-05 13:13:53 +0100

    Add div tag

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,6 +1,6 @@
 // JavaScript script to update text color of <header> element to red
 $(document).ready(function(){
-	$("#red_header").click(function(){
+	$("DIV#red_header").click(function(){
 		$("header").css("color", "#FF0000");
 	});
 });
