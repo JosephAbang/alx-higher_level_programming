f8980bd8c6005615bd513b1ffbf70b06b612096d
JosephAbang <abangjosef@gmail.com>
2024-05-05 13:30:57 +0100

    update text color of <header> element to red

---
+++ 0x15-javascript-web_jquery/100-script.js
@@ -0,0 +1,5 @@
+// update text color of <header> element to red
+document.addEventListener("DOMContentLoaded", function() {
+	var header = document.querySelector('header');
+	header.style.color = '#FF0000';
+});
